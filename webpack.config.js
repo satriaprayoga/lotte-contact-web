43e16f9ad6ff25c9339255db402640bb480415dc
Gilang Satria <satria.prayoga@gmail.com>
2018-07-15 02:08:10 +0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path=require('path');
+
+module.exports={
+    entry:'./app.js',
+    output:{
+        filename:'main.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(scss)$/,
+                use:[
+                    {
+                        loader:'style-loader',
+                    },
+                    {
+                        loader:'css-loader'
+                    },
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            plugins:function(){
+                                return[
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        loader:'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
